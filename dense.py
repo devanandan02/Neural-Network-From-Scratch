e4b087b2ad2a4cb51bc523183db0b0d1b6c19f42
Deva Nandan S <nandusajeev09@gmail.com>
2023-11-01 21:26:21 +0530

    dense updated

--- dense.py
+++ dense.py
@@ -1,18 +1,21 @@
 import numpy as np
-from layer import Layer
 
-class Dense(Layer):
+class Dense:
     def __init__(self, input_size, output_size):
-        self.weights = np.random.randn(output_size, input_size)
-        self.bias = np.random.randn(output_size, 1)
+        self.weights = np.random.rand(input_size, output_size)
+        self.biases = np.zeros((1, output_size))
 
-    def forward(self, input):
-        self.input = input
-        return np.dot(self.weights, self.input) + self.bias
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+        return self.output
 
-    def backward(self, output_gradient, learning_rate):
-        weights_gradient = np.dot(output_gradient, self.input.T)
-        input_gradient = np.dot(self.weights.T, output_gradient)
-        self.weights -= learning_rate * weights_gradient
-        self.bias -= learning_rate * output_gradient
-        return input_gradient
+    def backward(self, error, learning_rate):
+        weights_error = np.dot(self.inputs.T, error)
+        biases_error = np.sum(error, axis=0, keepdims=True)
+        input_error = np.dot(error, self.weights.T)
+
+        self.weights -= learning_rate * weights_error / len(self.inputs)
+        self.biases -= learning_rate * biases_error / len(self.inputs)
+
+        return input_error
